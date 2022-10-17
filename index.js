6a99245c83da240c8ab1de4d6626f0ccb933170e
nour khoury <khourynour79@gmail.com>
2022-10-17 17:41:24 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const { Body } = require("node-fetch");
+
+// Add your code here
+function submitData(usrName,usrEmail) {
+return fetch('http://localhost:3000/users',{
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+accept: "application/json"    
+},
+body: JSON.stringify({
+    name : usrName,
+    email : usrEmail})
+})
+.then(res => res.json())
+.then(data => {
+    document.querySelector("body").append(data.id)
+})
+.catch(error =>{
+    document.querySelector("body").append(error.message)
+})
+
+}
